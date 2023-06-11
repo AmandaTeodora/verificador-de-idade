ba8ef26de2371f7c7a26fa8dfa54bed6761cb826
Amanda <amanda_tos@hotmail.com>
2023-06-11 11:48:32 -0300

    ultimo commit

--- script.js
+++ script.js
@@ -13,11 +13,33 @@ function verificar() {
         img.setAttribute('id', 'foto')
         if (fsex[0].checked) {
             genero ='Homem'
-            
+            if (idade >=0 && idade <= 10){
+                img.setAttribute('src', 'meninonv.png')
+            } else if (idade < 21){
+                img.setAttribute('src', 'jovemh.png')
+
+            }else if (idade < 50){
+                img.setAttribute('src', 'homemnv.png')
+            }else {
+                img.setAttribute('src', 'idosonv.png')
+            }
+
 
         } else if (fsex[1].checked) {
             genero = 'Mulher'
+            if (idade >=0 && idade <= 10){
+                img.setAttribute('src', 'meninanv.png')
+            } else if (idade < 21){
+                img.setAttribute('src', 'jovem2.png')
+
+            }else if (idade < 50){
+                img.setAttribute('src', 'mulhernv.png')
+            }else {
+                img.setAttribute('src', 'idosanv.png')
+            }
         }
+        res.style.textAlign = 'center'
         res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
     }
